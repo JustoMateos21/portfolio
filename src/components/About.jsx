8c2a5594cce9cbac2072f7479fea87b9639d048c
JustoMateos21 <98715129+JustoMateos21@users.noreply.github.com>
2022-07-15 12:53:45 -0300

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className='text-4xl font-bold inline border-b-4
+          border-gray-500' >About</p>
+        </div>
+        <p className='text-xl mt-20'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+          veniam doloremque saepe tempora? Distinctio enim recusandae
+          reiciendis, pariatur cupiditate aperiam labore quos perspiciatis
+          dolores perferendis et tempora! Ad, ducimus placeat!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+          veniam doloremque saepe tempora? Distinctio enim recusandae
+          reiciendis, pariatur cupiditate aperiam labore quos perspiciatis
+          dolores perferendis et tempora! Ad, ducimus placeat!
+        </p>
+        <br />
+        <p className='text-xl'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+          veniam doloremque saepe tempora? Distinctio enim recusandae
+          reiciendis, pariatur cupiditate aperiam labore quos perspiciatis
+          dolores perferendis et tempora! Ad, ducimus placeat!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+          veniam doloremque saepe tempora? Distinctio enim recusandae
+          reiciendis, pariatur cupiditate aperiam labore quos perspiciatis
+          dolores perferendis et tempora! Ad, ducimus placeat!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
